21e7e003ba6b85cb8068e8943bbd58b8e16b61c6
Anurag Verma <vermaanurag@gmail.com>
2026-08-29 23:46:52 +0530

    Add edit feature and pie chart

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+let myChart;
 
 function addExpense() {
   let name = document.getElementById("expenseName").value;
@@ -40,19 +41,72 @@ function displayExpenses() {
     let li = document.createElement("li");
     li.innerHTML = expenses[i].name + " (" + expenses[i].category + ") - ₹" + expenses[i].amount + " - " + expenses[i].date;
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.innerHTML = "Delete";
-    deleteBtn.onclick = function() {
-      expenses.splice(i, 1);
-      saveToStorage();
-      displayExpenses();
-    };
+    let editBtn = document.createElement("button");
+editBtn.innerHTML = "Edit";
+editBtn.onclick = function() {
+  let newName = prompt("Naya naam:", expenses[i].name);
+  let newAmount = prompt("Naya amount:", expenses[i].amount);
 
-    li.appendChild(deleteBtn);
+  if (newName !== null && newAmount !== null && newName !== "" && Number(newAmount) > 0) {
+    expenses[i].name = newName;
+    expenses[i].amount = Number(newAmount);
+    saveToStorage();
+    displayExpenses();
+  }
+};
+
+let deleteBtn = document.createElement("button");
+deleteBtn.innerHTML = "Delete";
+deleteBtn.onclick = function() {
+  expenses.splice(i, 1);
+  saveToStorage();
+  displayExpenses();
+};
+
+li.appendChild(editBtn);
+li.appendChild(deleteBtn);
     list.appendChild(li);
 
     total += expenses[i].amount;
   }
 
   document.getElementById("totalAmount").innerHTML = total;
+
+  updateChart();
 }
+
+function updateChart() {
+  let categoryTotals = {};
+
+  for (let i = 0; i < expenses.length; i++) {
+    let cat = expenses[i].category;
+    let amt = expenses[i].amount;
+
+    if (categoryTotals[cat]) {
+      categoryTotals[cat] += amt;
+    } else {
+      categoryTotals[cat] = amt;
+    }
+  }
+
+  let labels = Object.keys(categoryTotals);
+  let data = Object.values(categoryTotals);
+
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  let ctx = document.getElementById("expenseChart");
+  myChart = new Chart(ctx, {
+    type: "pie",
+    data: {
+      labels: labels,
+      datasets: [{
+        data: data,
+        backgroundColor: ["#4CAF50", "#2196F3", "#FF9800", "#9C27B0"]
+      }]
+    }
+  });
+}
+
+displayExpenses();
